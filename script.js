7701b77dd67212a3ba827c8c075bf1d75ae32963
Karim Idrisov <karim.idrisow2016@gmail.com>
2021-09-18 10:25:13 +1000

    fix: add lock to body when burger open

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const burger = document.querySelector('.header__burger');
 const headerMenu = document.querySelector('.header__menu');
 const links = document.querySelectorAll('.header__link');
+const body = document.querySelector('body');
 function toggleActive() {
    burger.classList.toggle('active');
    headerMenu.classList.toggle('active');
+   body.classList.toggle('lock');
 }
 
 burger.addEventListener('click', toggleActive);
